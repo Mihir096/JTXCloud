984de5fd6579a114bb7ac5c5ed145bbfad57457d
Nimit Shah <nimit.shah@jarvistechnolabs.com>
2024-01-30 17:31:11 +0530

    Initial Commit

---
+++ JTXCloudDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JTXCloudDemo
+//
+//  Created by iOS Dev 01 on 30/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
